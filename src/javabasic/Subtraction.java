407ba0025c2b85eb7976cfccc57eb6e770a6c49e
pooja <poojasinghg0007@gmail.com>
2024-06-14 23:14:38 +0530

    subtraction method

---
+++ src/javabasic/Subtraction.java
@@ -0,0 +1,17 @@
+package javabasic;
+
+public class Subtraction {
+    public static void main(String[] args) {
+   Subtraction s1=new Subtraction();
+   s1.sub();
+   System.out.println(s1.sub());
+
+    }
+   public int sub() {
+        int a=50;
+        int b=40;
+        int c=a-b;
+        return c;}
+
+    }
+
